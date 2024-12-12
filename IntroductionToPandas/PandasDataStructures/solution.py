475a40626ca4ca4810754f8bbcd362cac75c172a
Kirill Gubkin <k.gubkin@list.ru>
2024-12-12 14:01:30 +0600

    add solution in IntroductionToPandas

--- IntroductionToPandas/PandasDataStructures/solution.py
+++ IntroductionToPandas/PandasDataStructures/solution.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-def createDataframe(student_data: List[List[int]]):
+def createDataframe(student_data: list[list[int]]):
     df = pd.DataFrame(student_data, columns = ['student_id', 'age'])
     return df
